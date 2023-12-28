20b8a2dd60eb42b05d470cc5f739c6c169d1532c
lwl <liwenlong@lingman.cn>
2023-12-28 09:38:28 +0800

    init

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+
+data class Shape(private var a : Int,private var b : Int){
+    public fun setA (a : Int) : Unit{
+        this.a = a;
+    }
+    public fun getA() : Int{
+        return this.a;
+    }
+}
+
+
+fun main(args: Array<String>) {
+    var testNull : Int?;
+    var shape = Shape(1,1)
+    shape.setA(2);
+    println(shape.getA())
+    var a  = 123
+    val b = 1234;
+    print(a);
+    print(a);
+    val c = '1'
+    println(c);
+    println("${b + 1}");
+
+}
