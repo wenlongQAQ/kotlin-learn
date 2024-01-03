a771d950bdcd04075484e2cf222463631b82d2ab
lwl <liwenlong@lingman.cn>
2024-01-03 11:11:09 +0800

    init

--- src/main/kotlin/bot/factory/BotInit.kt
+++ src/main/kotlin/bot/factory/BotInit.kt
@@ -1,6 +1,9 @@
 package bot.factory
 
+import java.io.ObjectInput
+
 class BotInit {
     val bot = BotFactory().createBot();
     val file : File = File("src/main/resources/commands.txt");
+    val obj : ObjectInput = ObjectInputStream(FileInputStream(file));
 }
